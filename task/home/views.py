afd60363ab08de1b738527b418a54c41e042aa2e
Akshata Hipparkar <akshatahipparkar88@gmail.com>
2026-03-12 15:27:18 +0530

    Added requirements.txt for deployment

--- task/home/views.py
+++ task/home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Task
+from rest_framework import viewsets
+from .serializers import TaskSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
 
 def index(request):
--- task/task/urls.py
+++ task/task/urls.py
@@ -1,33 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers, serializers, viewsets
-from home.models import Task
+from rest_framework import routers
 from home import views
 
-# Serializer
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Task
-        fields = '__all__'
-
-# API ViewSet
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-# Router
+# Router - Only point to the ViewSet now located in views.py
 router = routers.DefaultRouter()
-router.register(r'tasks', TaskViewSet)
+router.register(r'tasks', views.TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # UI routes
+    # UI routes (Your working frontend logic)
     path('', views.index, name='home'),
     path('add/', views.add_task),
     path('toggle/<int:id>/', views.toggle_task),
     path('delete/<int:id>/', views.delete_task),
 
-    # API routes
+    # API routes (Professional REST structure)
     path('api/', include(router.urls)),
 ]
